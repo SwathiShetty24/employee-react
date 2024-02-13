81e26b3009cc7ff4e0e32e3e63545cf07b771556
SwathiShetty24 <156899118+SwathiShetty24@users.noreply.github.com>
2024-02-13 14:30:21 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import Employee from './Employee';
 
 function App() {
   const emp=[
-    {id:'e1',name:'Swathi',doj:new Date(2020,8,24),exp:5},
-    {id:'e2',name:'Vaish',doj:new Date(2021,1,19),exp:5},
-    {id:'e3',name:'Varshini',doj:new Date(2021,10,6),exp:2},
-    {id:'e4',name:'Sahana',doj:new Date(2021,7,23),exp:2},
+    {id:'e1',name:'Swathi',doj:new Date(2020,8,24),exp:7},
+    {id:'e2',name:'Vaish',doj:new Date(2021,1,19),exp:12},
+    {id:'e3',name:'Varshini',doj:new Date(2021,10,6),exp:13},
+    {id:'e4',name:'Sahana',doj:new Date(2021,7,23),exp:15},
 ]
 
 
