9cc6fded0c88681368edead4dafea053ceac8dec
SwathiShetty24 <swathichandrappa2001@gmail.com>
2024-02-18 22:49:14 +0530

    with redux implementation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
-import Employee from './Employee';
+import logo from './logo.svg';
+import './App.css';
+import EmployeeDetail from './Emplyee/EmployeeDetail';
+import NewEmployee from './Emplyee/NewEmployee';
+import { useState } from 'react';
+import Employees from './Emplyee/Employees';
+import EmployeeFilter from './Emplyee/EmployeeFilter';
+import EmployeeForm from './Emplyee/EmployeeForm';
+import { EmployeeContext } from './Emplyee/EmployeeContext';
+import EmployeeContextProvider from './Emplyee/EmployeeContext';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect, useMemo} from 'react';
+import {sendExpenseData} from './store'
+import employeeStore from './store';
 
-function App() {
-  const emp=[
-    {id:'e1',name:'Swathi',doj:new Date(2020,8,24),exp:5},
-    {id:'e2',name:'Vaish',doj:new Date(2021,1,19),exp:5},
-    {id:'e3',name:'Varshini',doj:new Date(2021,10,6),exp:2},
-    {id:'e4',name:'Sahana',doj:new Date(2021,7,23),exp:2},
-]
+const App=()=> {
+  const items=useSelector(state=>state.items);
+  useEffect(
+    () => {
+      fetch('https://tricon-2e6b8-default-rtdb.firebaseio.com/employee.json', {
+        method:'PUT',
+        body: JSON.stringify(items)
+      })
+    }
+  )
+  const dispatch = useDispatch();
 
+  // useEffect(
+  //   () => {
+  //     dispatch(sendExpenseData(items))
+  //   },
+  //   [items, dispatch]
+  // );
 
   return (
-    <div>
-    <h1> Let's get started</h1>
-    <Employee items={emp}/>
-    </div>
+    <EmployeeContextProvider>
+      <div>
+        <NewEmployee>
+        <EmployeeForm/>
+        </NewEmployee>
+        <Employees/>
+      </div>
+      </EmployeeContextProvider>
   );
 }
 
